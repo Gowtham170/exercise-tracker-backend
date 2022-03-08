c8f4724014bedd9cd7021dca61c0ece1ce7f77d5
Gowtham <69344863+Gowtham170@users.noreply.github.com>
2022-03-08 20:33:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -18,6 +18,15 @@ const port = process.env.PORT;
 //middleware
 app.use(cors());
 app.use(express.json());
+app.use((req, res, next)=>{  
+    res.setHeader("Access-Control-Allow-Origin", "*");  
+    res.setHeader(  
+        "Access-Control-Allow-Headers",  
+        "Origin, X-Requested-With, Content-Type, Accept");  
+        res.setHeader("Access-Control-Allow-Methods",  
+        "GET, POST, PATCH, DELETE, OPTIONS");  
+    next();  
+}); 
 
 // connected to the mongoDB using the connection string
 mongoose.connect(connectionString, {
